c5831b5b047c4d196f6afef8dfa0bb4e1cc03ea0
jespmar <jesper.martinsson92@gmail.com>
2021-09-14 16:29:29 +0200

    Added logo to site settings

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -10,6 +10,11 @@ export default {
       title: "Title",
     },
     {
+      name: "logo",
+      type: "image",
+      title: "Logo",
+    },
+    {
       name: "description",
       type: "text",
       title: "Description",
