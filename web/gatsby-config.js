17a181e547dc503922fff9d33c05af0ee93ad3ad
jespmar <jesper.martinsson92@gmail.com>
2021-10-07 08:36:15 +0200

    Added netlify plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-netlify",
     {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/app/*`] },
